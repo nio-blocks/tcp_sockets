461ce9f7a1bf054b4ac12e79c34817d4b2574ae9
Tom Young <tyoung@n.io>
2016-11-03 09:10:05 -0600

    refactored to suck less, thanks chris

--- tcp_server_block.py
+++ tcp_server_block.py
@@ -19,50 +19,44 @@ class TCPserver(Block):
 
     def __init__(self):
         super().__init__()
-        self.conn = None
-        self.s = None
         self._kill = False
 
     def start(self):
         super().start()
         spawn(self._tcp_server)
 
-    def process_signals(self, signals):
-        for signal in signals:
-            resp = self.response(signal).encode('utf-8')
-            try:
-                self.conn.sendto(resp, self.client(signal))
-                self.logger.debug('sent response {}'.format(resp))
-            except:
-                self.logger.exception('failed to send response')
+    # def process_signals(self, signals):
+        # for signal in signals:
+            # resp = self.response(signal).encode('utf-8')
+            # try:
+                # self.conn.sendto(resp, self.client(signal))
+                # self.logger.debug('sent response {}'.format(resp))
+            # except:
+                # self.logger.exception('failed to send response')
 
     def stop(self):
         self._kill = True
-        if self.conn:
-            self.conn.close()
-            self.logger.debug('closed connection')
-        self.s.close()
-        self.logger.debug('closed socket')
         super().stop()
 
     def _tcp_server(self):
         self.logger.debug('started server thread')
-        buffer_size = 8192
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((self.IP_addr(), self.port()))
-        self.s.listen(1)
-        self.logger.debug('listening for connections')
-        self.conn, addr = self.s.accept()
-        self.logger.debug('{} connected'.format(addr))
-        while self._kill == False:
-            data = self.conn.recv(buffer_size).decode()
-            if data:
-                self.notify_signals([Signal({"data": data, "addr": addr})])
-            if not data:
-                self.logger.debug('no data, breaking')
-                self.conn.close()
-                self.logger.debug('closed connection')
-                self.s.close()
-                self.logger.debug('closed socket')
-                break
-        _tcp_server()
+        buffer_size = 1024
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.IP_addr(), self.port()))
+            s.listen(1)
+            while self._kill == False:
+                self.logger.debug('listening for connections')
+                conn, addr = s.accept()
+                self.logger.debug('{} connected'.format(addr))
+                with conn:
+                    while self._kill == False:
+                        data = conn.recv(buffer_size)
+                        self.logger.debug('received data {}'.format(data))
+                        if data:
+                            self.notify_signals([Signal(
+                                {"data": data, "addr": addr})])
+                        if not data:
+                            self.logger.debug(
+                                'connection closed by remote client '
+                                '{}'.format(addr))
+                            break
