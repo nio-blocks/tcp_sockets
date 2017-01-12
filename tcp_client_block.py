9250d7f5ccef5b77234a0575df3620283954a65e
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-12 14:40:25 -0700

    use visible instead of hidden on expect_response

--- tcp_client_block.py
+++ tcp_client_block.py
@@ -10,7 +10,7 @@ class TCPClient(Block):
     message = StringProperty(title='Message', default='GET / HTTP/1.1\n')
     port = IntProperty(title='Port', default=50001)
     expect_response = BoolProperty(
-        title='Expect response?', default=True, hidden=True)
+        title='Expect response?', default=True, visible=False)
     version = VersionProperty('0.0.1')
 
     def process_signals(self, signals):
