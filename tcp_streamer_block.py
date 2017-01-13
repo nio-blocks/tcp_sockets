beb15bccbe934c33dadf9ebd180f43a47779f875
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-13 15:41:06 -0700

    track only the connecting ip, not the port

--- tcp_streamer_block.py
+++ tcp_streamer_block.py
@@ -1,11 +1,13 @@
 import socket
 
 from nio.block.base import Block
+from nio.command import command
 from nio.properties import StringProperty, IntProperty, VersionProperty
 from nio.signal.base import Signal
 from nio.util.threading.spawn import spawn
 
 
+@command("current_connections", method='_list_connections')
 class TCPStreamer(Block):
 
     host = StringProperty(title='IP Address', default='127.0.0.1')
@@ -40,7 +42,7 @@ class TCPStreamer(Block):
                     self.logger.debug(
                         'connection closed by remote client '
                         '{}'.format(addr))
-                    self._conn_dict.pop(addr)
+                    self._conn_dict.pop(addr[0])
                     break
 
     def _tcp_server(self):
@@ -54,9 +56,9 @@ class TCPStreamer(Block):
                 conn, addr = s.accept()
 
                 # keep track of all accepted connections
-                if addr not in self._conn_dict:
+                if addr[0] not in self._conn_dict:
                     # new connection
-                    self._conn_dict.update({addr: conn})
+                    self._conn_dict.update({addr[0]: conn})
                 else:
                     # the address has already been connected to, and it never
                     # closed itself (likely due to power loss).
@@ -65,9 +67,17 @@ class TCPStreamer(Block):
 
                     # closing the connection should cause the thread used by
                     # that connection to exit
-                    self._conn_dict[addr].close()
-                    self._conn_dict[addr] = conn
+                    self.logger.debug('Accepted a connection from an address '
+                                      'already in use: {}, closing connection '
+                                      'object: {}'
+                                      .format(addr[0], self._conn_dict[addr[0]]))
+                    self._conn_dict[addr[0]].shutdown(1)
+                    self._conn_dict[addr[0]].close()
+                    self._conn_dict[addr[0]] = conn
 
                 self.logger.debug('{} connected'.format(addr))
 
                 spawn(self._recv, conn, addr, buffer_size)
+
+    def _list_connections(self):
+        return self._conn_dict
