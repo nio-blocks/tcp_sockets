3d6079cb0a402927057fe25fcf1060c248b91497
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-19 09:49:56 -0700

    refactor reconnect and no data tests

--- tests/test_tcp_streamer_block.py
+++ tests/test_tcp_streamer_block.py
@@ -54,7 +54,8 @@ class TestTCPStreamer(NIOBlockTestCase):
             mock_connect = MagicMock()
             mock_connect.recv.return_value = None
             mock_socket.return_value.__enter__.return_value.\
-                accept.return_value = (mock_connect, ('1.2.3.4', 12345))
+                accept.side_effect = [(mock_connect, ('1.2.3.4', 12345)),
+                                      Exception]
 
             blk.start()
 
@@ -64,47 +65,34 @@ class TestTCPStreamer(NIOBlockTestCase):
                 listen.assert_called_once_with(1)
 
             sleep(1)
-            # the connection won't be out of blk._connections due to accept
-            # always returning an identical connection, but close will have
-            # been called
-            mock_connect.close.assert_called_once_with()
+            self.assertEqual(len(blk._connections), 0)
 
-            blk.stop()
+            with self.assertRaises(Exception):
+                blk.stop()
 
+            # since no data is received, no signals should have been notified.
             self.assert_num_signals_notified(0)
 
     def test_reconnect(self):
-        """Once the block is started, it will start creating a bunch of
-        connections very quickly since accept is mocked to always return,
-        and putting them in blk._connections. Since all of these
-        connections are the same mocked connection object, close() will
-        be called on the previous connection and be removed from the
-        dict, replaced by the same connection object. So there should
-        only ever be one connection in the dict at a time if reconnect
-        functionality is working.
-        """
-
         blk = TCPStreamer()
         self.configure_block(blk, {'host': '1.2.3.4', 'port': 12345})
         with patch('socket.socket') as mock_socket:
             mock_connect = MagicMock()
-            mock_connect.recv.return_value = None
+            mock_connect.recv.return_value = 'datum'
             mock_socket.return_value.__enter__.return_value. \
-                accept.return_value = (mock_connect, ('1.2.3.4', 12345))
+                accept.side_effect = [(mock_connect, ('1.2.3.4', 12345)),
+                                      (mock_connect, ('1.2.3.4', 12345))]
 
             blk.start()
-            sleep(1.5)
-            self.assertGreater(mock_connect.close.call_count, 0)
+            sleep(1)
+
+            # this should always be one, as reconnect pops the old connection
+            # and adds the new one on the same host
+            self.assertEqual(len(blk._connections), 1)
+
             blk.stop()
 
-            # since no data is received, no signals should have been notified.
-            self.assert_num_signals_notified(0)
-            try:
-                self.assertEqual(len(blk._connections), 1)
-            except:
-                # we caught the block in the middle of removing a connection
-                # due to receiving no data. By this point, it is very
-                # likely that another connection has come in and has called
-                # close on the connection object, and this would be the case
-                # if reconnect is working
-                self.assertGreater(mock_connect.close.call_count, 0)
+            # since this block receives data, it won't close the connection
+            # itself. If close is called, it's because a new connection on the
+            # same host has connected.
+            self.assertGreater(mock_connect.close.call_count, 0)
