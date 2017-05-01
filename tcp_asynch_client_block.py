7a5b4e7c0168e647ecde4b47c57fb101ac41ede4
Tom Young <tyoung@n.io>
2017-05-01 08:54:44 -0600

    refactor block and test

--- tcp_asynch_client_block.py
+++ tcp_asynch_client_block.py
@@ -2,7 +2,7 @@ import socket
 
 from nio.block.base import Block
 from nio.signal.base import Signal
-from nio.properties import StringProperty, IntProperty, \
+from nio.properties import StringProperty, IntProperty, BoolProperty, \
                            VersionProperty, Property
 from nio.util.threading.spawn import spawn
 from nio.block.mixins import Retry
@@ -13,11 +13,14 @@ class TCPAsynchClient(Retry, Block):
     host = StringProperty(title='IP Address', default='127.0.0.1')
     message = Property(title='Message', default='GET / HTTP/1.1\n')
     port = IntProperty(title='Port', default=50001)
+    reconnect = BoolProperty(title='Reconnect when host closes connection',
+                             default=False)
     version = VersionProperty('0.0.1')
 
     def __init__(self):
         super().__init__()
-        self._main_thread = None
+        self._receive_thread = None
+        self._connect_thread = None
         self._kill = False
         self._conn = None
         self._buffer_size = 8192
@@ -25,15 +28,21 @@ class TCPAsynchClient(Retry, Block):
 
     def start(self):
         super().start()
-        self._connect()
-        self._main_thread = spawn(self._receive)
+        self._connect_thread = spawn(self.execute_with_retry, self._connect)
+        self._receive_thread = spawn(self._receive)
 
     def stop(self):
         self._kill = True
         try:
-            self._main_thread.join(1)
+            self._receive_thread.join(1)
         except:
-            self.logger.warning('main thread had already exited before join')
+            self.logger.warning(
+                'receive thread had already exited before join')
+        try:
+            self._connect_thread.join(1)
+        except:
+            self.logger.warning(
+                'connect thread had already exited before join')
         if self._conn:
             self._cleanup()
         super().stop()
@@ -41,19 +50,15 @@ class TCPAsynchClient(Retry, Block):
     def process_signals(self, signals):
         for signal in signals:
             message = self.message(signal)
-            self.execute_with_retry(self._send, message)
-
-    def before_retry(self, *args, **kwargs):
-        self.logger.warning('Failed to send {}, reconnecting'.format(
-            args
-        ))
-        self._main_thread.join(1)
-        if self._conn:
-            self._cleanup()
-        self._connect()
-        self._main_thread = spawn(self._receive)
+            if self._conn:
+                self._send(message)
+            else:
+                self.logger.error('Inactive connection, dropping signal')
+                self._connect_thread = spawn(self.execute_with_retry,
+                                             self._connect)
 
     def _connect(self):
+        self.logger.debug('Connecting ...')
         self._conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._host = (self.host(), self.port())
         self._conn.connect(self._host)
@@ -66,13 +71,18 @@ class TCPAsynchClient(Retry, Block):
 
     def _receive(self):
         while not self._kill:
-            data = self._conn.recv(self._buffer_size)
-            if not data:
-                self.logger.debug('Remote host closed connection')
-                self._conn = None
-                break
-            self.logger.debug('Received {}'.format(data))
-            self.notify_signals(Signal({'data': data, 'host': self._host}))
+            if self._conn:
+                data = self._conn.recv(self._buffer_size)
+                self.logger.debug('Received {}'.format(data))
+                self.notify_signals(Signal({'data': data, 'host': self._host}))
+                if not data:
+                    self.logger.debug('Remote host closed connection')
+                    self._conn = None
+                    if not self.reconnect():
+                        break
+                    else:
+                        self._connect_thread = spawn(self.execute_with_retry,
+                                                     self._connect)
 
     def _cleanup(self):
         self._conn.shutdown(2)
--- tests/test_tcp_asynch_client_block.py
+++ tests/test_tcp_asynch_client_block.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
@@ -7,39 +7,100 @@ from ..tcp_asynch_client_block import TCPAsynchClient
 
 class TestTCPAsynchClient(NIOBlockTestCase):
 
-    @patch('socket.socket')
-    def test_connection(self, mock_socket):
-        blk = TCPAsynchClient()
-        self.configure_block(blk, {})
-        blk.start()
-        mock_socket.return_value.connect.assert_called_with(
-            ('127.0.0.1', 50001))
-        blk.stop()
+    def test_connection(self):
+        with patch('socket.socket') as mock_socket:
+            blk = TCPAsynchClient()
+            self.configure_block(blk, {})
+            blk.start()
+            self.assertEqual(1, mock_socket.return_value.connect.call_count)
+            mock_socket.return_value.connect.assert_called_with(
+                ('127.0.0.1', 50001))
+            blk.stop()
 
-    @patch('socket.socket')
-    def test_process_signal(self, mock_socket):
-        blk = TCPAsynchClient()
-        self.configure_block(blk, {
-            'message': '{{ $hello }}\n',
-            'host': '1.2.3.4',
-            'port': '1234'}),
-        blk.start()
-        blk.process_signals([Signal({'hello': 'n.io'})])
-        mock_socket.return_value.connect.assert_called_with(('1.2.3.4', 1234))
-        mock_socket.return_value.send.assert_called_with('n.io\n')
-        blk.stop()
+    def test_connection_retry(self):
+        with patch('socket.socket') as mock_socket:
+            blk = TCPAsynchClient()
+            self.configure_block(blk, {})
+            mock_socket.return_value.connect.side_effect = [
+                Exception,
+                MagicMock(),
+            ]
+            blk.start()
+            from time import sleep
+            sleep(1.1)
+            self.assertEqual(2, mock_socket.return_value.connect.call_count)
+            mock_socket.return_value.connect.assert_called_with(
+                ('127.0.0.1', 50001))
+            blk.stop()
 
-    @patch('socket.socket')
-    def test_receive_message(self, mock_socket):
-        mock_socket.return_value.recv.return_value = 'message'
-        blk = TCPAsynchClient()
-        self.configure_block(blk, {})
-        blk.start()
-        blk.stop()
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {
-                'data': mock_socket.return_value.recv.return_value,
-                'host': ('127.0.0.1', 50001),
-            }
-        )
+    def test_process_signal(self):
+        with patch('socket.socket') as mock_socket:
+            blk = TCPAsynchClient()
+            self.configure_block(blk, {
+                'message': '{{ $hello }}\n',
+                'host': '1.2.3.4',
+                'port': '1234'}),
+            blk.start()
+            blk.process_signals([Signal({'hello': 'n.io'})])
+            mock_socket.return_value.connect.assert_called_with(
+                ('1.2.3.4', 1234))
+            mock_socket.return_value.send.assert_called_with('n.io\n')
+            blk.stop()
+
+    def test_process_signal_reconnect(self):
+        with patch('socket.socket') as mock_socket:
+            blk = TCPAsynchClient()
+            self.configure_block(blk, {
+                'message': '{{ $hello }}\n',
+                'host': '1.2.3.4',
+                'port': '1234'}),
+            mock_socket.return_value.connect.side_effect = Exception
+            blk.start()
+            blk.process_signals([Signal({'hello': 'n.io'})])
+            mock_socket.return_value.connect.assert_called_with(
+                ('1.2.3.4', 1234))
+            mock_socket.return_value.send.assert_called_with('n.io\n')
+            blk.stop()
+
+    def test_receive_message(self):
+        with patch('socket.socket') as mock_socket:
+            blk = TCPAsynchClient()
+            self.configure_block(blk, {})
+            mock_socket.return_value.recv.return_value = b'message'
+            blk.start()
+            self.assertDictEqual(
+                self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+                {
+                    'data': b'message',
+                    'host': ('127.0.0.1', 50001),
+                }
+            )
+            blk.stop()
+
+    def test_reconnect_on_close(self):
+        with patch('socket.socket') as mock_socket:
+            blk = TCPAsynchClient()
+            self.configure_block(blk, {'reconnect': True})
+            mock_socket.return_value.recv.side_effect = [
+                b'',
+                b'message'
+            ]
+            blk.start()
+            self.assertDictEqual(
+                self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+                {
+                    'data': b'',
+                    'host': ('127.0.0.1', 50001),
+                }
+            )
+            from time import sleep
+            sleep(1.1)
+            self.assertEqual(2, mock_socket.return_value.connect.call_count)
+            self.assertDictEqual(
+                self.last_notified[DEFAULT_TERMINAL][1].to_dict(),
+                {
+                    'data': b'message',
+                    'host': ('127.0.0.1', 50001),
+                }
+            )
+            blk.stop()
